4f5fe68e4436fa858c030781e0eb37b3506d9b90
杨郑林 <yangzhenglin@idongjia.cn>
2017-06-21 15:26:50 +0800

    add files

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+
+http.listen(3000, function () {
+    console.log('listening port at 3000');
+})
+
+var count = 0;
+
+//监听链接
+io.on('connection', function (socket) {
+    var addedUser = false;
+
+
+    //发送消息
+    socket.on('new message', function (data) {
+        //广播消息
+        socket.broadcast.emit('new message', {
+            username: socket.username,
+            message: data
+        });
+    });
+
+    //监听新用户
+    socket.on('add user', function (username) {
+        if (addedUser) return;
+
+        socket.username = username;
+        addedUser = true;
+        ++count;
+
+        socket.emit('login', {
+            message: 'Welcome to chat!',
+            count: count
+        });
+
+        //广播新用户加入
+        socket.broadcast.emit('user joined', {
+            username: socket.username,
+            count: count
+        });
+    });
+
+    // 监听并广播用户离线
+    socket.on('disconnect', function () {
+        if (addedUser) {
+            --count;
+            socket.broadcast.emit('user left', {
+                username: socket.username,
+                count: count
+            });
+        }
+    });
+});
---
+++ public/main.js
@@ -0,0 +1,159 @@
+$(function () {
+    var FADE_TIME = 150;
+    var COLORS = [
+        '#e21400', '#91580f', '#f8a700', '#f78b00',
+        '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
+        '#3b88eb', '#3824aa', '#a700ff', '#d300e7'
+    ];
+
+    var $window = $(window);
+    var $usernameInput = $('.usernameInput'); // username
+    var $messages = $('.messages'); // Messages
+    var $inputMessage = $('.inputMessage'); // message input
+
+    var $loginPage = $('.login.page'); // The login page
+    var $chatPage = $('.chat.page'); // The chatroom page
+
+    // $loginPage.fadeOut();
+    // $chatPage.show()
+    var username;
+    var connected = false;
+    var typing = false;
+    var lastTypingTime;
+    var $currentInput = $usernameInput.focus();
+
+    var socket = io();
+
+    function setUsername() {
+        username = $usernameInput.val().trim();
+
+        if (username) {
+            $loginPage.fadeOut();
+            $chatPage.show();
+            $loginPage.off('click');
+            $currentInput = $inputMessage.focus();
+
+            //向服务器广播添加新用户
+            socket.emit('add user', username);
+        }
+    }
+
+    //add message
+    function addMessage(data, options) {
+
+        var $usernameDiv = $('<span class="username"/>')
+            .text(data.username)
+            .css('color', setColor(data.username));
+        var $messageBodyDiv = $('<span class="messageBody">')
+            .text(data.message);
+
+        var typingClass = data.typing ? 'typing' : '';
+        var $messageDiv = $('<li class="message"/>')
+            .data('username', data.username)
+            .addClass(typingClass)
+            .append($usernameDiv, $messageBodyDiv);
+
+        addElement($messageDiv, options);
+    }
+
+    //add Element
+    function addElement(el, options) {
+        var $el = $(el);
+
+        if (!options) {
+            options = {};
+        }
+        if (typeof options.fade === 'undefined') {
+            options.fade = true;
+        }
+        if (typeof options.prepend === 'undefined') {
+            options.prepend = false;
+        }
+
+        if (options.fade) {
+            $el.hide().fadeIn(FADE_TIME);
+        }
+        if (options.prepend) {
+            $messages.prepend($el);
+        } else {
+            $messages.append($el);
+        }
+        $messages[0].scrollTop = $messages[0].scrollHeight;
+    }
+
+    // Send message
+    function sendMessage() {
+        var message = $inputMessage.val();
+        if (message && connected) {
+            $inputMessage.val('');
+            addMessage({
+                username: username,
+                message: message
+            });
+
+            //往服务器广播新消息时间
+            socket.emit('new message', message);
+        }
+    }
+
+    //print
+    function log(message, options) {
+        var $el = $('<li>').addClass('log').text(message);
+        addElement($el, options)
+
+    }
+
+    // Set color
+    function setColor(username) {
+        var hash = 7;
+        for (var i = 0; i < username.length; i++) {
+            hash = username.charCodeAt(i) + (hash << 5) - hash;
+        }
+        var index = Math.abs(hash % COLORS.length);
+        return COLORS[index];
+    }
+
+    $window.keydown(function (event) {
+        if (!(event.ctrlKey || event.metaKey || event.altKey)) {
+            $currentInput.focus();
+        }
+
+        if (event.which === 13) {
+            if (username) {
+                sendMessage();
+                // socket.emit('stop typing');
+                typing = false;
+            } else {
+                setUsername();
+            }
+        }
+    });
+
+    socket.on('login', function (data) {
+        $('#count').text(data.count);
+        connected = true;
+        log(data.message, {
+            prepend: true
+        });
+    })
+
+    socket.on('user joined', function (data) {
+        $('#count').text(data.count);
+        if (connected) {
+            log(data.username + ' joined');
+        }
+    })
+
+    socket.on('new message', function (data) {
+        if(connected){
+            addMessage(data);
+        }
+    })
+
+    socket.on('user left', function (data) {
+        $('#count').text(data.count);
+        if (connected) {
+            log(data.username + ' left');
+        }
+    })
+})
